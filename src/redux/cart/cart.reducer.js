e4eb184b5696baacd0a5cbad71a5d80eafefde6a
joshtru <joshuaojc@gmail.com>
2019-09-29 12:15:53 -0500

    Implemented redux to hold items added to cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
 import cartActionTypes from "./cart.types";
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 const cartreducer = (state = INITIAL_STATE, action) => {
@@ -10,6 +11,11 @@ const cartreducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         hidden: !state.hidden
+      };
+    case cartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload]
       };
 
     default:
